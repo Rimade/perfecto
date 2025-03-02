bb7713629e737011ced99b51b2dab01fe90f4f00
Muhammad-Amin <muhammad-amin4@mail.ru>
2025-03-02 21:25:27 +0300

    добавление redux

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,10 +1,31 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import cls from './Counter.module.scss';
+import { Button } from 'shared/ui/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { counterActions } from '../model/slice/counterSlice';
 
-interface CounterProps {
-    className?: string;
-}
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector((state: StateSchema) => state.counter.value);
 
-export const Counter = ({ className }: CounterProps) => (
-    <div className={classNames(cls.Counter, {}, [className])} />
-);
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+    return (
+        <div>
+            {/* eslint-disable-next-line i18next/no-literal-string */}
+            <h1>
+                value =
+                {counterValue}
+            </h1>
+            <Button onClick={increment}>
+                +
+            </Button>
+            <Button onClick={decrement}>
+                -
+            </Button>
+        </div>
+    );
+};
