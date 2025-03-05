25906a33d61c6ab518deb6302ba3c67cf47b1e76
Muhammad-Amin <muhammad-amin4@mail.ru>
2025-03-05 14:20:11 +0300

    добавил input, login, form ui

---
+++ src/shared/ui/Input/Input.tsx
@@ -0,0 +1,76 @@
+import {
+  ChangeEvent, InputHTMLAttributes, memo, useEffect, useRef, useState,
+} from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import styles from './Input.module.scss';
+
+type HTMLInputProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'>;
+
+interface InputProps extends HTMLInputProps {
+  className?: string;
+  value?: string | number;
+  autoFocus?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (value: string) => void;
+}
+
+export const Input = memo((props: InputProps) => {
+  const {
+    className, value, onChange, type = 'text', placeholder, autoFocus, ...otherProps
+  } = props;
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [caretPosition, setCaretPosition] = useState(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (autoFocus) {
+      inputRef.current.focus();
+      setIsFocused(true);
+    }
+  }, [autoFocus]);
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange?.(e.target.value);
+    setCaretPosition(e.target.value.length);
+  };
+
+  const onBlur = () => {
+    setIsFocused(false);
+  };
+
+  const onFocus = () => {
+    setIsFocused(true);
+  };
+
+  const onSelect = (e: any) => {
+    setCaretPosition(e?.target?.selectionStart || 0);
+  };
+
+  return (
+    <div className={classNames(styles.InputWrapper, {}, [className])}>
+      {placeholder && (
+        <div className={styles.placeholder}>
+          {`${placeholder}>`}
+        </div>
+      )}
+
+      <div className={styles.caretWrapper}>
+        <input
+          ref={inputRef}
+          type={type}
+          value={value}
+          onChange={onChangeHandler}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onSelect={onSelect}
+          className={classNames(styles.input, {}, [className])}
+          {...otherProps}
+        />
+        {isFocused && (
+          <span className={styles.caret} style={{ left: `${caretPosition * 8.4}px` }} />
+        )}
+      </div>
+    </div>
+  );
+});
